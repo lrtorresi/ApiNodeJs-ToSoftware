8e79cebe587aaeeb07f61e1e7ce39b9e4a49bfca
Rodrigo Torresi <lrtorresi@hotmail.com>
2020-04-03 00:29:44 -0300

    Refactoring Controller

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -4,17 +4,28 @@ const { uuid } = require('uuidv4'); // Criar Guid
 //Funções executadas (chamadas pelas Rotas)
 module.exports = {
 
+
     //Create User
     async create(request, response) {
         const Uuid = uuid();
         const { Nome, Cpf, Email, Password } = request.body;
 
+        //Verificando se o e-mail já esta cadastrado
+        const emailUser = await connection('user')
+            .where('Email', Email)
+            .orWhere('Cpf', Cpf)
+            .select('Nome')
+            .first();
+
+        if (emailUser != null) {
+            return response.status(401).json({ error: 'Operação não permitida! Usuario já cadastrados.' });
+        }
+        //usuario não encontrado e vai cadastrar
         await connection('user').insert({
             Uuid, Nome, Cpf, Email, Password,
         })
         return response.json({ Uuid, Nome, Cpf });
     },
-
 
     //Select All User
     async getAllUser(request, response) {
